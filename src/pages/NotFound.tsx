110291a4228b57ce4d2dbf935ceb5a5bef90aa09
Dwip Biswas <dwipbiswas@yahoo.com>
2026-02-23 19:50:25 +0530

    Added cursed 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,12 +4,12 @@ export default function NotFound() {
   return (
     <div className="min-h-screen bg-black flex flex-col items-center justify-center text-center"
       <img
-        src="/404-mobile.png"
+        src="/404-mobile.jpg"
         alt="404 Mobile"
         className="block md:hidden max-w-[90%] max-h-[70vh] object-contain"
       />
       <img
-        src="/404-desktop.png"
+        src="/404-desktop.jpg"
         alt="404 Desktop"
         className="hidden md:block max-w-[70%] max-h-[80vh] object-contain"
       />
