d87ee05d10f56cc1935d6db0141001845fe872e7
Dwip Biswas <dwipbiswas@yahoo.com>
2026-02-23 19:46:37 +0530

    Added cursed 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,15 +2,17 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen bg-black flex flex-col items-center justify-center text-center">
-      {/* 404 Image */}
+    <div className="min-h-screen bg-black flex flex-col items-center justify-center text-center"
       <img
-        src="/404.jpg"
-        alt="404 Not Found"
-        className="max-w-[90%] max-h-[70vh] object-contain"
+        src="/404-mobile.png"
+        alt="404 Mobile"
+        className="block md:hidden max-w-[90%] max-h-[70vh] object-contain"
       />
-
-      {/* Home link */}
+      <img
+        src="/404-desktop.png"
+        alt="404 Desktop"
+        className="hidden md:block max-w-[70%] max-h-[80vh] object-contain"
+      />
       <Link
         to="/"
         className="mt-6 text-green-500 hover:text-green-400 transition text-sm tracking-widest"
